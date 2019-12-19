bebcdde1bb1110f0514d4866486e073f6203686d
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-12-19 19:37:45 +0600

    Add project files.

---
+++ AsyncAwaitLesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncAwaitLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
